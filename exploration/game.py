0c9822bb9ea698a18d336efcb1df8f3fc2df7052
Kuresov <alex.kirsopp@gmail.com>
2018-11-27 16:03:33 -0500

    wip quick fix

--- exploration/game.py
+++ exploration/game.py
@@ -1,7 +1,7 @@
 # import exploration_button
 # import explor_game
 from time import sleep
-from random import randint 
+from random import randint
 
 DIO = 17
 CLK = 11
@@ -9,7 +9,7 @@ STB = 22
 
 
 def handle_buttons(button_num):
-	print("Button Handler -> Button pressed: ", button_num)
+        BUTTON_list.append(buttons)
 	time.sleep(1)
 
 def main():
@@ -31,14 +31,13 @@ def main():
 				LED_list.append(n)
 
 			explor_game.led_sequence(LED)
-			
+
 			while len(BUTTON_list) != (level+1):
 				buttons = exploration_button.TM1638_Button(DIO, CLK, STB)
 				print("Waiting for input...")
 				buttons.wait_for_input(handle_buttons)
-				BUTTON_list.append(buttons)
-			print(BUTTON_list)
 
+			print(BUTTON_list)
 
 			if LED_list == BUTTON_list: 	# Check if LED and Button sequences match.
 				completed = True
--- exploration_button.py
+++ exploration_button.py
@@ -5,12 +5,6 @@
 import time
 import pytm1638
 import RPi.GPIO as GPIO
-#GPIO.setwarnings(False)
-
-DIO = 17		# GPIO/BCM 17
-CLK = 11		# GPIO/BCM 11
-STB = 22		# GPIO/BCM 22
-
 
 class TM1638_Button:
     def __init__(self, dio, clk, stb):
@@ -33,32 +27,3 @@ class TM1638_Button:
 
         cb(keys)
         return keys
-
-
-#def button_press():
-#    global angle
-#
-#    while True:
-#        time.sleep(2)
-#        pressed = display.get_buttons()
-#        print("Inside Integration Button.")
-#        if pressed == 1:
-#        angle = 90
-#        display.set_led(0,1)
-#        elif pressed == 2:
-#        angle = 180
-#        display.set_led(1, 1)
-#        elif pressed == 4:
-#        angle = 270
-#        display.set_led(2, 1)
-#        elif pressed == 8:
-#        angle = 360
-#        display.set_led(3, 1)
-#
-#        print("Button pressed: ", pressed)
-#        print("Angle movement: ", angle)
-#
-#        if angle > 0:
-#            break
-#
-#	return angle
