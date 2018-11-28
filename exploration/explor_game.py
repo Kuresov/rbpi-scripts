47d91a0492510e8d97fa8095ce2a4a12204492cd
pilliplee <phillip0711@hotmail.com>
2018-11-28 01:55:01 -0500

    Memorization Game Complete

--- exploration/explor_game.py
+++ exploration/explor_game.py
@@ -9,15 +9,58 @@ display = pytm1638.TM1638(DIO, CLK, STB)
 
 display.enable(1)
 
-def led_sequence(lst):
+def lightup(pos):
+    display.set_led(pos-1,1)
+    sleep(0.1)
+    display.set_led(pos-1,0)
+    return None
+
+def led_sequence(lst, lvl):
+        speed_level = [1.5, 1.5, 1.0, 1.0, 0.75, 0.75, 0.5, 0.5, 0.25, 0.25]
+
 	for pos in lst:
-		display.set_led(pos, 1)
-		sleep(0.5)
-		display.set_led(pos, 0)
+            print(pos)
+	    display.set_led(pos-1, 1)
+	    sleep(speed_level[lvl])
+	    display.set_led(pos-1, 0)
+	    sleep(speed_level[lvl])
+	    
 	return None
 
-def button_sequence():
+def display_rdy():
+        display.send_char(0, 128)
+        display.send_char(1, 128) 
+        display.send_char(2, 128)   
+        display.send_char(3, 128)   
+        display.send_char(4, 128)  
+        display.send_char(5, 128)   
+        display.send_char(6, 128)  
+        display.send_char(7, 128)
+        return None
 
+def winner():
+    display.send_char(0, 63)
+    display.send_char(1, 92) 
+    display.send_char(2, 84)
+    display.send_char(3, 121)
+    return None
+
+def loser():
+    display.send_char(0, 56)
+    display.send_char(1, 63) 
+    display.send_char(2, 109)
+    display.send_char(3, 121)
+    return None
+
+
+def display_go():
+	num_byte = [63, 6, 91, 79, 102, 109, 125, 7, 127, 103]		# Values for: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+
+	display.send_char(0, 111)   # Display Segment 1
+	display.send_char(1, 99)   # Display Segment 2
+        sleep(2)	
+        return None
+    
 def display_score(lvl):
 	num_byte = [63, 6, 91, 79, 102, 109, 125, 7, 127, 103]		# Values for: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
 
@@ -27,7 +70,7 @@ def display_score(lvl):
 	# Display Segment 5
 	display.send_char(4, 56)
 	# Display Segment 6
-	display.send_char(5, 128)
+	display.send_char(5, 156)
 	# Display Segment 8
 	display.send_char(6, num_byte[lvl])
 
--- exploration/game.py
+++ exploration/game.py
@@ -1,53 +1,63 @@
-# import exploration_button
-# import explor_game
+import exploration_button
+import explor_game
 from time import sleep
 from random import randint
 
 DIO = 17
 CLK = 11
 STB = 22
-
+BUTTON_list = []
+START_LEVEL = 0
+FINAL_LEVEL = 10
 
 def handle_buttons(button_num):
-        BUTTON_list.append(buttons)
-	time.sleep(1)
+        BUTTON_list.append(button_num)
+        explor_game.lightup(button_num)
+        sleep(0.2)
+	return None
 
 def main():
-	print("CP320 - Exploration Project")
-	print("Prepared by: Alex Kirsopp & Pillip Lee")
-	final_level = 10
-	level = 0
-
+	print("CP320 - Exploration Project - Sequence Memorization Game")
+	print("Made by: Alex Kirsopp & Pillip Lee")
+	global completed
+        global level
 	try:
-		while level != final_level:
-			explor_game.display_score(level)
+		while START_LEVEL != FINAL_LEVEL:
+			explor_game.display_score(START_LEVEL)
 			completed = False
-			print("Welcome to Level: ", level)
+			print("Welcome to Level: ", START_LEVEL)
 			LED_list = []
+			global BUTTON_list
 			BUTTON_list = []
-
-			for x in range(level):
+                        sleep(3)
+			for x in range(START_LEVEL+1):
 				n = randint(1,8)
 				LED_list.append(n)
-
-			explor_game.led_sequence(LED)
-
-			while len(BUTTON_list) != (level+1):
+			explor_game.led_sequence(LED_list, START_LEVEL)
+			explor_game.display_go()
+			while True:
 				buttons = exploration_button.TM1638_Button(DIO, CLK, STB)
-				print("Waiting for input...")
+				print("Waiting for button press...")				
 				buttons.wait_for_input(handle_buttons)
-
-			print(BUTTON_list)
+				if len(BUTTON_list) == (START_LEVEL+1):
+                                    break
+                                else:
+                                    continue
 
 			if LED_list == BUTTON_list: 	# Check if LED and Button sequences match.
 				completed = True
-				level += 1
+				START_LEVEL += 1
 			else:
-				print("Program Done.")
+                                completed = False
+                                explor_game.loser()
+				print("Game Over.")
 				break
-
+			    
+		if completed == True:
+                    explor_game.winner()
+                    print("Game Completed! Congratulations!!")
+                        
 	except KeyboardInterrupt:
 		pass
-
 
 main()
